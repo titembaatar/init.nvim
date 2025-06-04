7e70cdb4f97e5f668eb24e931e3f807ef2bec518
titembaatar <etienne.guillemin@protonmail.com>
2025-06-05 00:58:09 +0200

    C rules for when I'm motivated

---
+++ after/ftplugins/c.lua
@@ -0,0 +1,10 @@
+-- Settings for Lua files
+local set = vim.opt_local
+
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.expandtab = true
+set.textwidth = 120     -- Recommended line length
+set.colorcolumn = "120" -- Show visual marker at column 120
+set.smartindent = true
