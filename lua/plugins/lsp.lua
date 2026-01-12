ba814aa010fbae9af38d5c7d8441758b72b03604
titembaatar <etienne.guillemin@protonmail.com>
2026-01-12 19:06:27 +0100

    lazydev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,4 +19,31 @@ return{
       "neovim/nvim-lspconfig",
     },
   },
+
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+
+  {
+    "saghen/blink.cmp",
+    opts = {
+      fuzzy = { implementation = "lua" },
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
 }
