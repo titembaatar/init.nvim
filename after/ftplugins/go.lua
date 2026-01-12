6dac50b67333fb97484e8dd11e07970283678aec
titembaatar <etienne.guillemin@protonmail.com>
2026-01-12 01:24:01 +0100

    it wasn't working...

--- after/ftplugins/go.lua
+++
@@ -1,9 +0,0 @@
--- Settings for Go files (following Go community conventions)
-local set = vim.opt_local
-
-set.tabstop = 4
-set.shiftwidth = 4
-set.softtabstop = 0
-set.expandtab = false   -- Use tabs (Go standard)
-set.textwidth = 100     -- Common line length for Go
-set.colorcolumn = "100" -- Show visual marker
