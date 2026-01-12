03819acbb1b8b3978be98a48f498ea94794c5fa2
titembaatar <etienne.guillemin@protonmail.com>
2026-01-12 01:24:58 +0100

    init

--- init.lua
+++ init.lua
@@ -2,4 +2,5 @@ _G.Util = require("util")
 require("config.options")     -- General Neovim options
 require("config.keymaps")     -- Key mappings
 require("config.lazy")        -- Plugin manager
+require("config.filetypes")   -- File types autocmd
 require("config.colorscheme") -- Color scheme configuration
