88c3a9fc60fb02d912099fe49e28be6e0f93ea8b
titembaatar <etienne.guillemin@protonmail.com>
2026-01-13 22:52:02 +0100

    cleaner, change for bash

--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -5,16 +5,95 @@ local function set_opts(opts)
 end
 
 local ft_settings = {
-  lua = { tabstop = 2, shiftwidth = 2, softtabstop = 2, textwidth = 120, colorcolumn = "120", expandtab = true, smartindent = true },
-  sh = { tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true, textwidth = 100, colorcolumn = "100", smartindent = true },
-  go = { tabstop = 4, shiftwidth = 4, softtabstop = 0, expandtab = false, textwidth = 100, colorcolumn = "100" },
-  c = { tabstop = 4, shiftwidth = 4, softtabstop = 4, expandtab = true, textwidth = 120, colorcolumn = "120", smartindent = true },
-  markdown = { tabstop = 2, shiftwidth = 2, softtabstop = 2, textwidth = 80, colorcolumn = "80", expandtab = true, conceallevel = 0, spell = true, spelllang = "en_us" },
-  json = { tabstop = 2, shiftwidth = 2, softtabstop = 2, expandtab = true, textwidth = 0, foldmethod = "syntax", foldenable = false },
-  yaml = { tabstop = 2, shiftwidth = 2, softtabstop = 2, textwidth = 80, colorcolumn = "80", expandtab = true },
-  toml = { tabstop = 2, shiftwidth = 2, softtabstop = 2, textwidth = 80, colorcolumn = "80", expandtab = true },
-  html = { tabstop = 2, shiftwidth = 2, softtabstop = 2, textwidth = 100, colorcolumn = "100", expandtab = true, smartindent = true },
-  css = { tabstop = 2, shiftwidth = 2, softtabstop = 2, textwidth = 100, colorcolumn = "100", expandtab = true, smartindent = true },
+  lua = {
+    tabstop = 2,
+    shiftwidth = 2,
+    softtabstop = 2,
+    textwidth = 120,
+    colorcolumn = "120",
+    expandtab = true,
+    smartindent = true
+  },
+  sh = {
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 4,
+    expandtab = true,
+    textwidth = 80,
+    colorcolumn = "80",
+    smartindent = true
+  },
+  go = {
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 0,
+    expandtab = false,
+    textwidth = 100,
+    colorcolumn = "100"
+  },
+  c = {
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 4,
+    expandtab = true,
+    textwidth = 120,
+    colorcolumn = "120",
+    smartindent = true
+  },
+  markdown = {
+    tabstop = 2,
+    shiftwidth = 2,
+    softtabstop = 2,
+    textwidth = 80,
+    colorcolumn = "80",
+    expandtab = true,
+    conceallevel = 0,
+    spell = true,
+    spelllang = "en_us"
+  },
+  json = {
+    tabstop = 2,
+    shiftwidth = 2,
+    softtabstop = 2,
+    expandtab = true,
+    textwidth = 0,
+    foldmethod = "syntax",
+    foldenable = false
+  },
+  yaml = {
+    tabstop = 2,
+    shiftwidth = 2,
+    softtabstop = 2,
+    textwidth = 80,
+    colorcolumn = "80",
+    expandtab = true
+  },
+  toml = {
+    tabstop = 2,
+    shiftwidth = 2,
+    softtabstop = 2,
+    textwidth = 80,
+    colorcolumn = "80",
+    expandtab = true
+  },
+  html = {
+    tabstop = 2,
+    shiftwidth = 2,
+    softtabstop = 2,
+    textwidth = 100,
+    colorcolumn = "100",
+    expandtab = true,
+    smartindent = true
+  },
+  css = {
+    tabstop = 2,
+    shiftwidth = 2,
+    softtabstop = 2,
+    textwidth = 100,
+    colorcolumn = "100",
+    expandtab = true,
+    smartindent = true
+  },
 }
 
 vim.api.nvim_create_autocmd("FileType", {
