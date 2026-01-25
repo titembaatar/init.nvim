f5e976936c067bb3840fff7de0aa755f431b5218
titembaatar <etienne.guillemin@protonmail.com>
2026-01-25 02:20:39 +0100

    refactor/mini: new plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,71 +1,43 @@
 return {
-  {
-    "nvim-mini/mini.nvim",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("mini.ai").setup({})
-      require("mini.bracketed").setup({})
-      require("mini.bufremove").setup({})
-      require("mini.cursorword").setup({})
-      require("mini.icons").setup({})
-      require("mini.pairs").setup({})
-      require('mini.sessions').setup({})
-      require("mini.tabline").setup({})
-      require("mini.trailspace").setup({})
+	{
+		'nvim-mini/mini.nvim',
+		version = '*',
+		config = function()
+			-- text
+			require("mini.ai").setup({})
+			require("mini.align").setup({})
+			require("mini.pairs").setup({})
+			require("mini.splitjoin").setup({})
+			require("mini.surround").setup({})
+			-- general
+			require("mini.bracketed").setup({})
+			require("mini.files").setup({})
+			require("mini.diff").setup({ view = { style = "sign" } })
+			-- ui
+			require("mini.icons").setup({})
+			require("mini.statusline").setup({ use_icons = true, })
+			require("mini.tabline").setup({})
+			require("mini.indentscope").setup({
+				draw = { animation = function() return 0 end, },
+				symbol = "│",
+			})
+			require("mini.hipatterns").setup({
+				highlighters = {
+					todo       = {pattern = "%f[%w]()TODO()%f[%W]",       group = "MiniHipatternsTodo"},
+					note       = {pattern = "%f[%w]()NOTE()%f[%W]",       group = "MiniHipatternsNote"},
+					fixme      = {pattern = "%f[%w]()FIXME()%f[%W]",      group = "MiniHipatternsFixme"},
+					hack       = {pattern = "%f[%w]()HACK()%f[%W]",       group = "MiniHipatternsHack"},
+					deprecated = {pattern = "%f[%w]()DEPRECATED()%f[%W]", group = "MiniHipatternsDeprecated"},
+				},
+			})
+		end
+	},
+	{
+		'nvim-mini/mini.trailspace',
+		event = 'BufReadPost',
+		opts = {},
+	},
 
-      require("mini.comment").setup({
-        options = {
-          ignore_blank_line = true,
-        },
-      })
-
-      require("mini.files").setup({
-        options = {
-          show_hidden = true,
-        },
-      })
-
-      require("mini.hipatterns").setup({
-        highlighters = {
-          todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-          fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-          hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-          note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-          deprecated = { pattern = "%f[%w]()DEPRECATED()%f[%W]", group = "MiniHipatternsDeprecated" },
-        },
-      })
-
-      require("mini.indentscope").setup({
-        draw = {
-          animation = function()
-            return 0
-          end,
-        },
-        mappings = {
-          object_scope = "",
-          object_scope_with_border = "",
-          goto_top = "",
-          goto_bottom = "",
-        },
-        symbol = "│",
-      })
-
-      require("mini.statusline").setup({
-        use_icons = true,
-      })
-
-      require("mini.surround").setup({
-        mappings = {
-          add = "gsa",
-          delete = "gsd",
-          find = "gsf",
-          find_left = "gsF",
-          highlight = "gsh",
-          replace = "gsr",
-          update_n_lines = "gsn",
-        },
-      })
-    end,
-  },
+	vim.keymap.set("n", "<leader>e",  "<Cmd>lua MiniFiles.open()<CR>",          { desc = "Mini File Explorer" }),
+	vim.keymap.set("n", "<leader>go", "<Cmd>lua MiniDiff.toggle_overlay()<CR>", { desc = "Toggle MiniDiff" })
 }
