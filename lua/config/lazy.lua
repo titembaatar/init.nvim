8bbba6f779b36f88d5d257bb5abdabd4b26e6916
titembaatar <etienne.guillemin@protonmail.com>
2026-01-25 02:19:27 +0100

    refactor/lazy: ?

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,61 +1,28 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system( {
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        -- "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-    cache = {
-      enabled = true,
-    },
-    reset_packpath = true,
-  },
-  install = { colorscheme = { "habamax" } },
-  checker = {
-    enabled = true,
-    frequency = 86400, -- check once a day
-    notify = false,
-  },
-  change_detection = {
-    enabled = true,
-    notify = false,
-  },
-  ui = {
-    size = { width = 0.8, height = 0.8 },
-    wrap = true,
-    border = "rounded",
-  },
-  profiling = {
-    enable = false,
-  },
+	spec = { { import = "plugins" }, },
+	checker = { enabled = true },
 })
