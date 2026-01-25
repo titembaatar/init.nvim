db8ea02b9adab87e477444a3069d368c28707c26
titembaatar <etienne.guillemin@protonmail.com>
2026-01-25 02:23:17 +0100

    refactor/treesitter: update to nvim0.11

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,47 +1,26 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    dependencies = {},
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        modules = {},
-        ignore_install = {},
-        ensure_installed = {
-          "lua",
-          "vim",
-          "vimdoc",
-          "bash",
-          "json",
-          "yaml",
-          "markdown",
-          "python",
-          "go",
-          "git_rebase",
-          "gitcommit",
-          "gitignore",
-          "query",
-        },
-        sync_install = false,
-        auto_install = false,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-        },
-      })
-
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-      vim.opt.foldenable = false
-    end,
-  },
+	{
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
+		build = ':TSUpdate',
+		config = function()
+			local ts = require("nvim-treesitter")
+			ts.install({
+				"c", "go", "lua",
+				"bash", "zsh",
+				"json", "yaml", "toml",
+				"desktop", "dockerfile",
+				"sway", "tmux",
+				"html", "css", "gotmpl",
+			})
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function(args)
+					local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+					if lang and vim.treesitter.query.get(lang, "highlights") then
+						vim.treesitter.start(args.buf)
+					end
+				end,
+			})
+		end,
+	}
 }
