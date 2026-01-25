58359c02ec33aed5d12587cd1b2a085703c13a59
titembaatar <etienne.guillemin@protonmail.com>
2026-01-25 02:18:24 +0100

    refactor/filetypes: streamlined with new options

--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -1,116 +1,69 @@
-local function set_opts(opts)
-  for k, v in pairs(opts) do
-    vim.opt_local[k] = v
-  end
-end
-
-local ft_settings = {
-  lua = {
-    tabstop = 2,
-    shiftwidth = 2,
-    softtabstop = 2,
-    textwidth = 120,
-    colorcolumn = "120",
-    expandtab = true,
-    smartindent = true
-  },
-  sh = {
-    tabstop = 4,
-    shiftwidth = 4,
-    softtabstop = 4,
-    expandtab = true,
-    textwidth = 80,
-    colorcolumn = "80",
-    smartindent = true
-  },
-  go = {
-    tabstop = 4,
-    shiftwidth = 4,
-    softtabstop = 0,
-    expandtab = false,
-    textwidth = 100,
-    colorcolumn = "100"
-  },
-  c = {
-    tabstop = 4,
-    shiftwidth = 4,
-    softtabstop = 4,
-    expandtab = true,
-    textwidth = 120,
-    colorcolumn = "120",
-    smartindent = true
-  },
-  markdown = {
-    tabstop = 2,
-    shiftwidth = 2,
-    softtabstop = 2,
-    textwidth = 80,
-    colorcolumn = "80",
-    expandtab = true,
-    conceallevel = 0,
-    spell = true,
-    spelllang = "en_us"
-  },
-  json = {
-    tabstop = 2,
-    shiftwidth = 2,
-    softtabstop = 2,
-    expandtab = true,
-    textwidth = 0,
-    foldmethod = "syntax",
-    foldenable = false
-  },
-  yaml = {
-    tabstop = 2,
-    shiftwidth = 2,
-    softtabstop = 2,
-    textwidth = 80,
-    colorcolumn = "80",
-    expandtab = true
-  },
-  toml = {
-    tabstop = 2,
-    shiftwidth = 2,
-    softtabstop = 2,
-    textwidth = 80,
-    colorcolumn = "80",
-    expandtab = true
-  },
-  html = {
-    tabstop = 2,
-    shiftwidth = 2,
-    softtabstop = 2,
-    textwidth = 100,
-    colorcolumn = "100",
-    expandtab = true,
-    smartindent = true
-  },
-  css = {
-    tabstop = 2,
-    shiftwidth = 2,
-    softtabstop = 2,
-    textwidth = 100,
-    colorcolumn = "100",
-    expandtab = true,
-    smartindent = true
-  },
+local lang = {
+	go = {
+		tabstop = 4,
+		shiftwidth = 4,
+	},
+	lua = {
+		tabstop = 4,
+		shiftwidth = 4,
+	},
+	html = {
+		tabstop = 2,
+		shiftwidth = 2,
+		softtabstop = 2,
+		expandtab = true,
+		textwidth = 100,
+		colorcolumn = "101",
+	},
+	css = {
+		tabstop = 2,
+		shiftwidth = 2,
+		softtabstop = 2,
+		expandtab = true,
+		textwidth = 100,
+		colorcolumn = "101",
+	},
+	markdown = {
+		tabstop = 2,
+		shiftwidth = 2,
+		softtabstop = 2,
+		expandtab = true,
+		conceallevel = 0,
+		spell = true,
+		spelllang = "en_us"
+	},
+	yaml = {
+		tabstop = 2,
+		shiftwidth = 2,
+		softtabstop = 2,
+		expandtab = true,
+	},
+	toml = {
+		tabstop = 2,
+		shiftwidth = 2,
+		softtabstop = 2,
+		expandtab = true,
+	},
+	json = {
+		tabstop = 2,
+		shiftwidth = 2,
+		softtabstop = 2,
+		expandtab = true,
+		textwidth = 0,
+	},
 }
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = vim.tbl_keys(ft_settings),
-  callback = function(args)
-    local settings = ft_settings[args.match]
-    if settings then
-      set_opts(settings)
-    end
+local function set_opts(o)
+	for k, v in pairs(o) do
+		vim.opt_local[k] = v
+	end
+end
 
-    if args.match == "html" then
-      vim.opt_local.matchpairs:append("<:>")
-    elseif args.match == "json" then
-      local fname = vim.fn.expand("%:t")
-      if fname == "tsconfig.json" or fname == "package.json" or fname:match("%.jsonc$") then
-        vim.opt_local.commentstring = "// %s"
-      end
-    end
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = vim.tbl_keys(lang),
+	callback = function(args)
+		if lang[args.match] then
+			set_opts(lang[args.match])
+		end
+	end,
 })
