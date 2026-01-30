425cef10a9be610f1e406311f207c564f9d82c29
titembaatar <etienne.guillemin@protonmail.com>
2026-01-31 00:04:37 +0100

    Revert "using default"
    This reverts commit 2fe0b359cb5a5d94243836334b9394e48eaa9410.

--- lua/plugins/sarnai.lua
+++ lua/plugins/sarnai.lua
@@ -3,5 +3,13 @@ return {
 		dir = "/home/titem/personal/sarnai.nvim",
 		lazy = false,
 		priority = 1000,
+		opts = {
+			cache = true,
+			styles = {
+				italic = false,
+				bold = false,
+				underline = false,
+			}
+		},
 	},
 }
