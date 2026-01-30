459ee4b57a216cb7c7a93b9b419ed135ff410abb
titembaatar <etienne.guillemin@protonmail.com>
2026-01-31 00:04:59 +0100

    using default

--- lua/plugins/sarnai.lua
+++ lua/plugins/sarnai.lua
@@ -3,13 +3,6 @@ return {
 		dir = "/home/titem/personal/sarnai.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {
-			cache = true,
-			styles = {
-				italic = false,
-				bold = false,
-				underline = false,
-			}
-		},
+		opts = {},
 	},
 }
